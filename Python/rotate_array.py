47e7436a916139896c7cecd802775db1a12ce8df
Priya <priyapanky@gmail.com>
2018-12-13 21:20:25 +0530

    Create rotate_array.py

---
+++ Python/rotate_array.py
@@ -0,0 +1,47 @@
+import array
+
+A = [1,2,3,4,5,6]
+B = [9,382,1,4,5,6]
+
+#rotating by using temp array
+def rotate_by_x(Arr, sizeArr, x):
+    tempArr = []
+    for i in range(x):
+        tempArr.append(Arr[i])
+    for i in range(sizeArr-x):
+        Arr[i] = Arr[x+i]
+    for i in range(x):
+        Arr[x+i] = tempArr[i]
+    return Arr
+
+#method by rotating x times
+def rotate_by_m(Arr, sizeArr, m):
+    for i in range(m-1):
+        rotate(A,sizeArr)
+    return Arr
+
+def rotate(Arr,sizeArr):
+    temp = Arr[0]
+    for i in range(sizeArr - 1):
+        Arr[i] = Arr[i + 1]
+    Arr[sizeArr-1] = temp
+    return Arr
+
+
+#print(A)
+#print(rotate_by_x(A,6,3))
+#print(B)
+#print(rotate(B,6))
+
+
+
+#your code in python
+newarr = [1,2,3,4,5,6,7,8,9,10]
+rotate_times = int(input("How many times do you want to rotate array?: "))
+
+for i in range(rotate_times):
+    temp = newarr[0]
+    for j in range(newarr.__len__()-1):
+        newarr[j] = newarr[j+1]
+    newarr[newarr.__len__()-1] = temp
+print(newarr)
