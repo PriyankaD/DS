e842a387ecd3e33e2902358d7543971812fdf34f
Priya <priyapanky@gmail.com>
2018-12-13 21:58:46 +0530

    added new file

---
+++ Python/rotate.py
@@ -0,0 +1,11 @@
+#your code in python
+newarr = [1,2,3,4,5,6,7,8,9,10]
+rotate_times = int(input("How many times do you want to rotate array?: "))
+
+for i in range(rotate_times):
+    temp = newarr[0]
+    for j in range(newarr.__len__()-1):
+        newarr[j] = newarr[j+1]
+    newarr[newarr.__len__()-1] = temp
+print(newarr)
+#test
